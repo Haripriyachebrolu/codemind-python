9066bb8be3bbab1bbcdef156e0d8a2906a014013
Haripriyachebrolu <111487498+Haripriyachebrolu@users.noreply.github.com>
2022-10-16 08:46:27 +0530

    n=int(input()) a=n+1 while True:     if (str(a)==str(a)[::-1]):         is_palindrome=True         break     else:         a+=1 b=n-1 while True:     if (str(b)==str(b)[::-1]):         is_palindrome=True         break     else:         b-=1 d1=a-n d2=n-b if d1>d2:     print(b) elif d1==d2:     print(b,a,end=' ') else:     print(a)

---
+++ Closest_Palindrome_.py
@@ -0,0 +1,23 @@
+n=int(input())
+a=n+1
+while True:
+    if (str(a)==str(a)[::-1]):
+        is_palindrome=True
+        break
+    else:
+        a+=1
+b=n-1
+while True:
+    if (str(b)==str(b)[::-1]):
+        is_palindrome=True
+        break
+    else:
+        b-=1
+d1=a-n
+d2=n-b
+if d1>d2:
+    print(b)
+elif d1==d2:
+    print(b,a,end=' ')
+else:
+    print(a)
